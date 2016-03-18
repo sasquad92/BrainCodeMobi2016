10246ae874e58d9c147836087073bf666d8b1366
sasquad92 <krzysztof.sommerrey@gmail.com>
2016-03-18 19:12:00 +0100

    Gorilla test

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
-    "fmt"
-   // "net/http"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
+func SayHelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func Greet(w http.ResponseWriter, r *http.Request) {
+	name := mux.Vars(r)["name"]
+	w.Write([]byte(fmt.Sprintf("Hello %s !", name)))
+}
+
+func ProcessPathVariables(w http.ResponseWriter, r *http.Request) {
+
+	// break down the variables for easier assignment
+	vars := mux.Vars(r)
+	name := vars["name"]
+	job := vars["job"]
+	age := vars["age"]
+	w.Write([]byte(fmt.Sprintf("Name is %s ", name)))
+	w.Write([]byte(fmt.Sprintf("Job is %s ", job)))
+	w.Write([]byte(fmt.Sprintf("Age is %s ", age)))
+}
+
 func main() {
-    fmt.Println("Hello No Name Heroes!")
+
+	mx := mux.NewRouter()
+
+	mx.HandleFunc("/", SayHelloWorld)
+	mx.HandleFunc("/{name}", Greet)
+
+	//to handle URL like
+	//http://website:8080/person/Boo/CEO/199
+
+	//http://website:8080/person/Boo/CEO/199 <- if age > 199, will cause 404 error
+
+	mx.HandleFunc("/person/{name}/{job}/{age:[0-199]+}", ProcessPathVariables)
+
+	http.ListenAndServe(":8080", mx)
 }
